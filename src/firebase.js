bcf69c233f8db9d442b209b4842276e62a5e8e36
Karan V <karanvknarayanan@gmail.com>
2020-08-30 12:44:16 +0530

    cleanup and setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCAjVqR_PxxnWyBVBTEmjVNKdmFftvVup4",
+    authDomain: "whatsapp-clone-aug.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-aug.firebaseio.com",
+    projectId: "whatsapp-clone-aug",
+    storageBucket: "whatsapp-clone-aug.appspot.com",
+    messagingSenderId: "515806144328",
+    appId: "1:515806144328:web:756c204716e3b73a12757d",
+    measurementId: "G-HWGQGMY03T"
+  };
